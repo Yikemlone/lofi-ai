ee75aea2168714b521bbfd7ec7d26e07ada58758
Yikemlone <sirvictini@gmail.com>
2024-03-23 00:11:45 +0000

    Added more details to chords retruned from the server

--- server.py
+++ server.py
@@ -6,12 +6,11 @@ app = Flask(__name__)
 CORS(app)
 
 ai_model = ChordGenerator()
-ai_model.init()
+ai_model.set_up()
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     chord_qty = int(request.args.get('chord_qty'))
-    chords = ai_model.generate_chords(chord_qty)
+    scale_key = request.args.get('scale_key')
+    chords = ai_model.generate_chords(chord_qty) # Add scale_key as an argument later
     return jsonify(chords)
-
-    # scale_key = request.args.get('scale_key')
